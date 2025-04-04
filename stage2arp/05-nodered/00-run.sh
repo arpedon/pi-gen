228d18d7c9b92f304f06126b0afde89e0bd0ce51
Antonis Kalipetis <akalipetis@withlogic.co>
2025-04-04 11:11:35 +0300

    fix(nodered): fix NodeRed installation

--- stage2arp/05-nodered/00-run.sh
+++ stage2arp/05-nodered/00-run.sh
@@ -2,3 +2,4 @@
 
 ls -la /tmp ${ROOTFS_DIR}/tmp
 install -m 744 files/update-nodejs-and-nodered.sh "${ROOTFS_DIR}/tmp/"
+install -m 644 files/environment "${ROOTFS_DIR}/home/pi/.node-red/environment"
--- stage2arp/05-nodered/01-run-chroot.sh
+++ stage2arp/05-nodered/01-run-chroot.sh
@@ -2,11 +2,14 @@
 
 set -ex
 
-/tmp/update-nodejs-and-nodered.sh --confirm-root --confirm-install --confirm-pi --no-init --node18
-
 mkdir -p /home/pi/.node-red && cd /home/pi/.node-red
+chown -R pi:pi /home/pi/.node-red
+/tmp/update-nodejs-and-nodered.sh --confirm-root --nodered-user=pi --confirm-install --confirm-pi --no-init --node18
 npm install node-red-dashboard \
   node-red-contrib-modbus \
   node-red-contrib-opcua \
   node-red-node-daemon \
   bcryptjs
+chown -R pi:pi /home/pi/.node-red
+
+systemctl enable nodered.service
