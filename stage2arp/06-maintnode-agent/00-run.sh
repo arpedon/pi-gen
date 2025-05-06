b027bc7c4d857187079183e7e1385153e5173845
Tasos Sangiotis <tsangiotis@arpedon.com>
2025-05-06 14:59:27 +0300

    temporary maintnode-agent.service

--- stage2arp/06-maintnode-agent/00-run.sh
+++ stage2arp/06-maintnode-agent/00-run.sh
@@ -6,6 +6,9 @@ mkdir -p "${ROOTFS_DIR}/usr/local/share/maintnode-setup"
 install -m 644 files/maintnode-agent.service "${ROOTFS_DIR}/usr/local/share/maintnode-setup/maintnode-agent.service"
 install -m 755 files/maintnode-setup.sh "${ROOTFS_DIR}/usr/local/bin/maintnode-setup"
 
+# as a temporary workaround, we need to copy the maintnode-agent.service file to /etc/systemd/system
+install -m 644 files/maintnode-agent.service "${ROOTFS_DIR}/etc/systemd/system/maintnode-agent.service"
+
 on_chroot << EOF
 cd /tmp
 tar -xzf maintnode-agent.tar.gz
