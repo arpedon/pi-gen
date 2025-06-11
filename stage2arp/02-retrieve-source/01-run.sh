9e9d3bd0dcba3ecdced4d07d7392dd5985f7b3ee
Antonis Kalipetis <akalipetis@withlogic.co>
2025-06-11 15:59:21 +0300

    chore(cleanup): cleanup comments

--- stage2arp/02-retrieve-source/01-run.sh
+++ stage2arp/02-retrieve-source/01-run.sh
@@ -3,8 +3,6 @@ set -ex
 
 rm -rf ${ROOTFS_DIR}/home/$FIRST_USER_NAME/maintnode ${ROOTFS_DIR}/home/$FIRST_USER_NAME/maintnode-local-db ${ROOTFS_DIR}/home/$FIRST_USER_NAME/daqhats
 export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-# git clone git@github.com:arpedon/maintnode.git "${ROOTFS_DIR}/home/$FIRST_USER_NAME/maintnode"
-# git clone git@github.com:arpedon/maintnode-local-db.git "${ROOTFS_DIR}/home/$FIRST_USER_NAME/maintnode-local-db"
 # Get the v2 branch for now
 git clone -b v2 git@github.com:arpedon/maintnode.git "${ROOTFS_DIR}/home/$FIRST_USER_NAME/maintnode"
 git clone -b v2 git@github.com:arpedon/maintnode-local-db.git "${ROOTFS_DIR}/home/$FIRST_USER_NAME/maintnode-local-db"
@@ -14,4 +12,3 @@ chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}/maintnod
 chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}/maintnode-local-db
 chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}/daqhats
 EOF
-
